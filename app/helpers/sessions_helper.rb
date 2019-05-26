37d3839977ab9eef8aa472e9aa2aeed148d779b8
ayu <goldayushi@gmail.com>
2019-05-27 00:55:32 +0900

    feat: Add session logout and login style.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,8 +4,18 @@ module SessionsHelper
 	end
 
 	def current_user
-		if session[:user_id] = user.id
+		if session[:user_id]
 			@current_user ||= User.find_by(id: session[:user_id])
 		end
 	end
+
+	def logged_in?
+		!current_user.nil?
+	end
+
+	# 現在のユーザーをログアウトする
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
